b8a586408642a30d2e3e9d24954e4efb1c7cc9b0
AL13F13 <dsauposafa@gmail.com>
2015-04-29 14:52:54 -0500

    cinco
    se cambio el config y acciones navigator

--- js/acciones.js
+++ js/acciones.js
@@ -6,7 +6,7 @@ document.addEventListener("deviceready",function(){
 	navigator.vibrate (1000);
 	
 	$ ('#btnbeep').on ('tap', function(){
-		navigator.beep(1);	
+		navigator.notification.beep(1)	
 		
 		$('#izquierda').on ('swipeleft', function (){
 			alert ("barrio a la izquierda");
