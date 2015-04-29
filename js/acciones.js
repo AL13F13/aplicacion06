954d80dd9ca1dcb1ff6e0555ad15e4b5ce2d30e1
AL13F13 <dsauposafa@gmail.com>
2015-04-29 13:51:24 -0500

    cuatro
    cambie acciones

--- js/acciones.js
+++ js/acciones.js
@@ -2,6 +2,25 @@
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	
+	$('#btnvibrar').on('tap', function(){
+	navigator.vibrate (1000);
+	
+	$ ('#btnbeep').on ('tap', function(){
+		navigator.beep(1);	
+		
+		$('#izquierda').on ('swipeleft', function (){
+			alert ("barrio a la izquierda");
+			
+			$ ('#derecha').on ('swiperight', function (){
+				alert ("barrio a la derecha");
+			
+			
+			
+
+});//derecha
+});// izquierda		
+});//tap btnbeep
+});//tap a btnvibrar
 }); 
 });
 
