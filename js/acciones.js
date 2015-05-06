a3016839fd6e5dadf809b7b44bfa866d6212b72f
AL13F13 <dsauposafa@gmail.com>
2015-05-06 14:44:43 -0500

    siete
    cambie el portrait y acciones

--- js/acciones.js
+++ js/acciones.js
@@ -4,37 +4,37 @@ document.addEventListener("deviceready",function(){
 	
 	$('#btnvibrar').on('tap', function(){
 	navigator.vibrate (1000);
-	
+	});	
 	$ ('#btnbeep').on ('tap', function(){
 		navigator.notification.beep(1)	
-		
+		});//Reinicio	
 		$('#izquierda').on ('swipeleft', function (){
 			alert ("barrio a la izquierda");
-			
+			});//Pausa
 			$ ('#derecha').on ('swiperight', function (){
 				alert ("barrio a la derecha");
-				
+				});//derecha
 				document.addEventListener("pause",function () {
 					
 					$('#listado').append("<p>se pauso</p>");
-					
+					});// izquierda	
 					document.addEventListener("resume",function () {
 						
 						$('#listado').append("<p>se reinicio</p>");
-						
+						});//tap btnbeep
 						  $(window).on ('orientationchange',function (e){
 							  
 							  $('#listado').append("<p>orientacion: "+ e.orientation +"</p>");
 			
-			
-			
-});			
-});//Reinicio			
-});//Pausa
-});//derecha
-});// izquierda		
-});//tap btnbeep
 });//tap a btnvibrar
+			
+			
+		
+		
+
+
+	
+
 }); 
 });
 
